6936fd4cd79c0cca09a8c483ce285cb021560025
unmyr <56982541+unmyr@users.noreply.github.com>
2020-09-19 15:29:49 +0900

    feat: add server and test codes

---
+++ test/test_index.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const supertest = require('supertest');
+
+const app = require("../index");
+
+describe("GET /", function() {
+  it("it should has status code 200", function(done) {
+    supertest(app)
+      .get("/")
+      .expect(200)
+      .end(function(err, res){
+        if (err) done(err);
+        assert.strictEqual(res.text, 'Hello World!');
+        done();
+      });
+  });
+});
