61b0870bebad67dea4aa37f00aa548ef3f01b768
unmyr <56982541+unmyr@users.noreply.github.com>
2021-10-03 00:23:44 +0900

    test: Apply Prettier.

--- test/test_index.js
+++ test/test_index.js
@@ -1,14 +1,14 @@
 const assert = require('assert');
 const supertest = require('supertest');
 
-const app = require("../index");
+const app = require('../index');
 
-describe("GET /", function() {
-  it("it should has status code 200", function(done) {
+describe('GET /', function () {
+  it('it should has status code 200', function (done) {
     supertest(app)
-      .get("/")
+      .get('/')
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
         assert.strictEqual(res.text, 'Hello World!');
         done();
